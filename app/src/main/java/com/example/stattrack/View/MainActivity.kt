46043f5e76fe75cc83ad5d7f8ba00a390eacb382
SimonF <s195479@student.dtu.dk>
2021-10-11 13:23:48 +0200

    Indledende opsætning af player-klasser, samt funktionalitet til navigering mellem fragments.

--- app/src/main/java/com/example/stattrack/View/MainActivity.kt
+++ app/src/main/java/com/example/stattrack/View/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.stattrack.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.annotation.ContentView
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,5 +13,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val playerFragment = fragment_spiller()
+
+        // Initialize start fragment.
+//        supportFragmentManager.beginTransaction().apply {
+//            replace(R.id.flfragmentTest, playerFragment)
+//            commit()
+//        }
+
+
+        // Initialize Fragment from button
+        val btn = findViewById<Button>(R.id.btnFragmentSpiller)
+        btn.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flfragmentTest, playerFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
+
+
