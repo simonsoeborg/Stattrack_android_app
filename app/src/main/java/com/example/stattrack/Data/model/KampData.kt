66621519940f7e4ee12bd1fdf9a768b1f67f856f
Kasper Baun <kasperbaun@hotmail.com>
2021-11-04 11:13:34 +0100

    Kamp, event, spillerstats oprettet

---
+++ app/src/main/java/com/example/stattrack/Data/model/KampData.kt
@@ -0,0 +1,13 @@
+package com.example.stattrack.Data.model
+
+import java.util.*
+
+data class KampData (
+    val Id : Int,
+    val CreatorId : String,
+    val CreatorTeamId : Int,
+    val Modstander : String,
+    val KampDato : Date,
+    val CreatorTeamGoals : Int,
+    val ModstanderGoals : Int
+)
---
+++ app/src/main/java/com/example/stattrack/Data/model/SpillerStats.kt
@@ -0,0 +1,15 @@
+package com.example.stattrack.Data.model
+
+data class SpillerStats (
+    val Id : Int,
+    val SpillerId : Int,
+    val Tidspunkt : String,
+    val Attempts : Int,
+    val Goals : Int,
+    val KeeperSaves : Int,
+    val Assists : Int,
+    val Mins2 : Int,
+    val Gulekort : Int,
+    val Roedekort : Int,
+    val KampId : Int
+        )
