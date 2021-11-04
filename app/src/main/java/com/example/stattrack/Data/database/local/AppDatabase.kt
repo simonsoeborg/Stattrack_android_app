99ac0a25865ad7ca2282e27ca31d66879eb4d466
IceMonk3y <fridolf.lindhard@gmai.com>
2021-11-04 12:05:03 +0100

    appDatBase update

--- app/src/main/java/com/example/stattrack/Data/database/local/AppDatabase.kt
+++ app/src/main/java/com/example/stattrack/Data/database/local/AppDatabase.kt
@@ -4,19 +4,20 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.stattrack.Data.database.Entity.PlayerEntity
-import com.example.stattrack.Data.database.Entity.TeamEntity
-import com.example.stattrack.Data.database.local.logik.PlayerDao
-import com.example.stattrack.Data.database.local.logik.TeamDao
+import com.example.stattrack.Data.database.Entity.*
+import com.example.stattrack.Data.database.local.logik.*
 
 @Database(
-    entities = [PlayerEntity::class, TeamEntity::class ],
+    entities = [PlayerEntity::class, TeamEntity::class, PlayerStatsEntity::class, MatchDataEntity::class, EventDataEntity::class ],
     version = 1,
     exportSchema = true
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun PlayerDao(): PlayerDao
     abstract fun TeamDao(): TeamDao
+    abstract fun PlayerStatsDao(): PlayerStatsDao
+    abstract fun MatchDataDao(): MatchDataDao
+    abstract fun EventDataDao(): EventDataDao
 
     companion object {
         fun build(context: Context): AppDatabase {
