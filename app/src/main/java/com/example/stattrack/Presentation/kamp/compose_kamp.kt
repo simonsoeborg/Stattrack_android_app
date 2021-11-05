d6fc4f49577781bdb24355a8913d5505552a49ae
Simon <s173255@student.dtu.dk>
2021-11-05 12:01:14 +0100

    EventComponent

--- app/src/main/java/com/example/stattrack/Presentation/kamp/compose_kamp.kt
+++ app/src/main/java/com/example/stattrack/Presentation/kamp/compose_kamp.kt
@@ -1,6 +1,7 @@
 package com.example.stattrack.Presentation.kamp
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -13,12 +14,15 @@ import androidx.compose.material.icons.filled.StopCircle
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.stattrack.Presentation.ui.theme.PrimaryBlue
+import com.example.stattrack.Presentation.ui.theme.PrimaryWhite
+import java.util.concurrent.CountDownLatch
 
 @Composable
 fun KampScreen() {
@@ -143,19 +147,122 @@ fun TeamComponent(hold1_name: String, hold2_name: String, hold1_sc: String, hold
 }
 
 @Composable
+fun DropdownMenu(
+    colorSelected: Color = PrimaryBlue,
+    colorBackground: Color = PrimaryWhite,
+    expanded: Boolean,
+    selectedIndex: Int,
+    items: List<EventItems>,
+    onSelect: (Int) -> Unit,
+    onDismissRequest: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Box {
+        content()
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = onDismissRequest,
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth()
+                .background(
+                    color = colorBackground,
+                    shape = RoundedCornerShape(16.dp)
+                )
+        ) {
+            items.forEachIndexed { index, s ->
+                if (selectedIndex == index) {
+                    DropdownMenuItem(modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            color = colorSelected,
+                            shape = RoundedCornerShape(16.dp)
+                        ),
+                        onClick = { onSelect(index) }
+                    ) {
+                        Text( text = s.title,
+                            color = PrimaryBlue,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth(0.9F)
+                            )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
 fun EventComponent() {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedIndex by remember { mutableStateOf(0) }
+    var eventItems = listOf(
+        EventItems.EventGoal,
+        EventItems.EventAttempt,
+        EventItems.EventSave,
+        EventItems.EventAssist,
+        EventItems.EventEjection,
+        EventItems.EventYellow,
+        EventItems.EventRed
+    )
+
+    var buttonTitle = eventItems[selectedIndex].title
+
+
     Column( modifier = Modifier
         .fillMaxWidth()
         .padding(10.dp)) {
         Row(
             modifier = Modifier
                 .align(CenterHorizontally)
+                .padding(2.dp)
         ) {
             OutlinedButton(onClick = { /*TODO*/ },
                 modifier = Modifier.size(100.dp),
                 shape = CircleShape,
                 border = BorderStroke(1.dp, PrimaryBlue)) {
                 Icon(Icons.Default.Add, contentDescription = "New Event", tint = PrimaryBlue)
+            }
+        }
+        Row() {
+            Column(modifier = Modifier.border(BorderStroke(1.dp, PrimaryBlue))) {
+                Row() {
+                    // Select Player
+                    DropdownMenu(
+                        colorSelected = PrimaryBlue,
+                        expanded = expanded,
+                        selectedIndex = selectedIndex,
+                        items = eventItems,
+                        onSelect = { index ->
+                            selectedIndex = index
+                            expanded = false
+                        },
+                        onDismissRequest = {
+                            expanded = false
+                        })
+                    {
+                        Button(
+                            onClick = {
+                                expanded = true
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                                .background(PrimaryWhite)
+                                .border(BorderStroke(1.dp, PrimaryBlue))
+                        ) {
+                            Text(
+                                text = buttonTitle,
+                                color = PrimaryBlue,
+                                maxLines = 1
+                            )
+                        }
+                    }
+                }
+                Row() {
+                    Column(modifier = Modifier.weight(1f)) {
+                    }
+                    Column(modifier = Modifier.weight(1f)) {
+                    }
+                }
             }
         }
     }
