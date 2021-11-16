671a683d6fcf38eec22c6ecb399a16ab9dbd4dfd
SimonF <s195479@student.dtu.dk>
2021-11-16 18:42:10 +0100

    Team kan nu vise faktiske lister af hold.

--- app/src/main/java/com/example/stattrack/presentation/hold/compose_hold.kt
+++ app/src/main/java/com/example/stattrack/presentation/hold/compose_hold.kt
@@ -1,14 +1,30 @@
 package com.example.stattrack.presentation.hold
 
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.stattrack.model.model.Team
+import com.example.stattrack.model.model.defaultTeamDummyData
 import com.example.stattrack.presentation.ui.theme.PrimaryBlue
+import kotlin.coroutines.coroutineContext
 
 @Composable
 fun HoldScreen() {
@@ -28,7 +44,7 @@ fun HoldScreen() {
                     ) {
                         Text(text = "Hold oversigt", fontSize = 32.sp, color = PrimaryBlue)
                         Column(modifier = Modifier.padding(10.dp)) {
-                            dummydata1()
+                            TeamList(defaultTeamDummyData)
                         }
                     }
                 }
@@ -57,6 +73,30 @@ fun dummydata1() {
         Text(text = "$item", modifier = Modifier.padding(2.dp), color = PrimaryBlue)
     }
 }
+
+
+@Composable
+fun TeamList(teams: List<Team> ) {
+    LazyColumn() {
+        items(
+            items = teams,
+            key = { team ->
+                // Return a stable + unique key for the item
+                team.teamId
+            },
+
+        ) {
+                    team ->
+            Surface(modifier = Modifier.clickable { println(team.name + team.teamId)}){
+                    Text(team.name,modifier = Modifier.padding(2.dp), color = PrimaryBlue)
+            }
+        }
+    }
+}
+
+
+
+
 
 @Composable
 fun dummydata2() {
