9da4bd62d20d4dc95693992b8384109e7e29f421
IceMonk3y <fridolf.lindhard@gmai.com>
2021-11-18 21:37:27 +0100

    Testoh

--- app/src/main/java/com/example/stattrack/presentation/hold/TeamViewState.kt
+++ app/src/main/java/com/example/stattrack/presentation/hold/TeamViewState.kt
@@ -4,8 +4,8 @@ import com.example.stattrack.model.model.Team
 
 /** [TeamViewState] This data class represents the view state for the Match screen. */
 data class TeamViewState (
-    val teams: List<Team> = emptyList(),
-    val players: List<Player> = emptyList()
+    var teams: List<Team> = emptyList(),
+    var players: List<Player> = emptyList()
 ) {
     val showLoading: Boolean
         get() = teams.isEmpty() && players.isEmpty()
--- app/src/main/java/com/example/stattrack/presentation/match/MatchViewState.kt
+++ app/src/main/java/com/example/stattrack/presentation/match/MatchViewState.kt
@@ -7,9 +7,9 @@ import com.example.stattrack.model.model.Team
 
 /** [MatchViewState] This data class represents the view state for the Match screen. */
 data class MatchViewState(
-    val teams: List<Team> = emptyList(),
-    val players: List<Player> = emptyList(),
-    val matchData: List<MatchData> = emptyList()
+    var teams: List<Team> = emptyList(),
+    var players: List<Player> = emptyList(),
+    var matchData: List<MatchData> = emptyList()
     /* TODO: Do we need to create login  for the trainer? val loggedInUser: Trainer */
 ) {
     val showLoading: Boolean
