229c0ca7d0cb49dd8c9409a2427ccc8f36b579f9
SimonF <s195479@student.dtu.dk>
2021-11-25 09:23:01 +0100

    Forsøg på viewModel og viewState

--- app/src/main/java/com/example/stattrack/presentation/player/PlayerViewModel.kt
+++ app/src/main/java/com/example/stattrack/presentation/player/PlayerViewModel.kt
@@ -4,6 +4,8 @@ package com.example.stattrack.presentation.player
 import androidx.lifecycle.*
 import com.example.stattrack.model.database.Repository
 import com.example.stattrack.model.model.*
+import com.example.stattrack.presentation.match.MatchViewState
+import com.example.stattrack.presentation.team.TeamViewState
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
@@ -13,80 +15,21 @@ import kotlinx.coroutines.launch
  * for the view to render the relevant information
  */
 class PlayerViewModel(private val repository: Repository) : ViewModel() {
-    /* cold-flow way of binding ui to viewmodel */
-    /*
-    val matchState: Flow<MatchViewState> = repository.getPlayerByName("asd")
-        .combine(repository.getTeamByName("asda")) { player, team ->
-            MatchViewState(teams = listOf(team), players = listOf(player))
-        }
 
-    val matchStateTest: Flow<MatchViewState> = repository.getAllPlayers()
-        .combine(repository.getAllTeams()) { player, team ->
-            MatchViewState(teams = team, players = player)
-        }
-
-
-    /* Hot-flow way of binding UI to ViewModel */
-    private val matchViewState = MatchViewState(
-        defaultTeamDummyData,
-        defaultDummyPlayerData,
-        defaultDummyMatchData
-    )
-    private val _hotMatchState = MutableStateFlow(matchStateTest)
-    val hotMatchState = _hotMatchState.asStateFlow()
-    */
-
-    // View-layer has no way of seeing this.
-    private val _viewState = MutableStateFlow(PlayerViewState())
+    private val _viewState = MutableStateFlow<List<PlayerStats>>(emptyList())
 
     // Read-only for the view-layer
-    val viewState: StateFlow<PlayerViewState> = _viewState
+    val viewState: StateFlow<PlayerViewState> = PlayerViewState(_viewState)
 
-    init {
-        /* Fetch data from DB when init so it is ready for use later on
-        *  Use viewState.value in Compose */
-        loadAllTeams()
-        loadAllPlayers()
-        loadAllMatchData()
-        loadAllEventData()
-        loadAllPlayerStats()
+
+    private fun loadAllPlayerStats(playerId : Int) {
+        viewModelScope.launch() {
+            repository.getPlayerStatsById(playerId).collect {
+                _viewState.value
+            }
+        }
     }
 
-    private fun loadAllTeams() {
-        viewModelScope.launch() {
-            repository.getAllTeams().collect{
-                _viewState.value.teams=it
-            }
-        }
-    }
-    private fun loadAllPlayers() {
-        viewModelScope.launch() {
-            repository.getAllPlayers().collect {
-                _viewState.value.players = it
-            }
-        }
-    }
-    private fun loadAllMatchData() {
-        viewModelScope.launch() {
-            repository.getAllMatchData().collect {
-                _viewState.value.matchData = it
-            }
-        }
-    }
-    private fun loadAllEventData() {
-        viewModelScope.launch() {
-            repository.getAllEvents().collect {
-                _viewState.value.eventData = it
-            }
-        }
-    }
-    private fun loadAllPlayerStats() {
-        viewModelScope.launch() {
-            repository.getAllPlayerStats().collect {
-                _viewState.value.playerStats = it
-            }
-        }
-    }
 }
 
 
--- app/src/main/java/com/example/stattrack/presentation/player/PlayerViewState.kt
+++ app/src/main/java/com/example/stattrack/presentation/player/PlayerViewState.kt
@@ -5,13 +5,5 @@ import com.example.stattrack.model.model.*
 
 /** [PlayerViewState] This data class represents the view state for the Player screen. */
 data class PlayerViewState(
-    var teams: List<Team> = emptyList(),
-    var players: List<Player> = emptyList(),
-    var matchData: List<MatchData> = emptyList(),
-    var eventData: List<EventData> = emptyList(),
-    var playerStats: List<PlayerStats> = emptyList()
-
-) {
-    val showLoading: Boolean
-        get() = teams.isEmpty() && players.isEmpty()
-}
+    val playerStats: List<PlayerStats> = emptyList()
+)
