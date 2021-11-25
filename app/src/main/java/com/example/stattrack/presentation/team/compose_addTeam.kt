29bc338eda21e3a7e3697486ea64d398f3a3a1b1
Kasper Baun <kasperbaun@hotmail.com>
2021-11-25 09:25:34 +0100

    work on AddTeam layout in compose

--- app/src/main/java/com/example/stattrack/presentation/team/compose_addTeam.kt
+++ app/src/main/java/com/example/stattrack/presentation/team/compose_addTeam.kt
@@ -1,26 +1,26 @@
 package com.example.stattrack.presentation.team
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.stattrack.model.model.Team
 import com.example.stattrack.presentation.navbar.Screen
+import com.example.stattrack.presentation.ui.theme.PrimaryBlue
+import com.example.stattrack.presentation.ui.theme.PrimaryWhite
+import com.example.stattrack.presentation.ui.theme.Typography
 
 
 @Composable
-fun AddTeam(navController: NavHostController, onSubmitPressed: (Team) -> Unit ) {
-    var placeholderClubName = "Not updated yet"
+fun AddTeam(navController: NavHostController?, onSubmitPressed: (Team) -> Unit ) {
+    var placeholderClubName = TextFieldValue("Klub navn")
     var placeholderTeamName = "Not updated yet"
     var placeholderCreatorName = "Not updated yet"
     var placeholderTeamUYear = "Not updated yet"
@@ -28,55 +28,87 @@ fun AddTeam(navController: NavHostController, onSubmitPressed: (Team) -> Unit ) 
 
     // TODO : Remember to create unique ID in DB when passing event to ViewModel
 
-    Column( modifier = Modifier.fillMaxWidth()){
-        Row(modifier = Modifier.padding(all = 10.dp)) {
+    Column( modifier = Modifier
+        .fillMaxWidth()
+        .background(color = PrimaryWhite)
+    ){
+        Row(modifier = Modifier
+            .padding(all = 10.dp)
+            .align(Alignment.CenterHorizontally)
+        ) {
             OutlinedTextField(
-                value = "Klub navn",
+                value = placeholderClubName,
                 onValueChange = { placeholderClubName = it },
-                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
+                textStyle = Typography.body1.copy(
+                textAlign = TextAlign.Center,
+                color = PrimaryBlue)
             )
         }
-        Row(modifier = Modifier.padding(all = 10.dp)) {
+        Row(modifier = Modifier
+            .padding(all = 10.dp)
+            .align(Alignment.CenterHorizontally)
+        ) {
             OutlinedTextField(
                 value = "Hold navn",
                 onValueChange = { placeholderTeamName = it },
-                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
+                textStyle = Typography.body1.copy(
+                    textAlign = TextAlign.Center,
+                    color = PrimaryBlue)
             )
         }
-        Row() {
+        Row(modifier = Modifier
+            .padding(all = 10.dp)
+            .align(Alignment.CenterHorizontally)
+        ) {
             OutlinedTextField(
                 value = "Træner navn",
                 onValueChange = { placeholderCreatorName = it },
-                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
+                textStyle = Typography.body1.copy(
+                    textAlign = TextAlign.Center,
+                    color = PrimaryBlue)
                 )
         }
-        Row(modifier = Modifier.padding(all = 10.dp)) {
+        Row(modifier = Modifier
+            .padding(all = 10.dp)
+            .align(Alignment.CenterHorizontally)
+        ) {
             OutlinedTextField(
                 value = "Årgang",
                 onValueChange = { placeholderTeamUYear = it },
-                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
+                textStyle = Typography.body1.copy(
+                    textAlign = TextAlign.Center,
+                    color = PrimaryBlue)
                 )
         }
-        Row(modifier = Modifier.padding(all = 10.dp)) {
+        Row(modifier = Modifier
+            .padding(all = 10.dp)
+            .align(Alignment.CenterHorizontally)
+        ) {
              OutlinedTextField(
                  value = "Division",
                  onValueChange = { placeholderDivision = it },
-                 textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
+                 textStyle = Typography.body1.copy(
+                     textAlign = TextAlign.Center,
+                     color = PrimaryBlue)
              )
          }
-        Row(modifier = Modifier.padding(all = 10.dp)) {
-            Button(onClick = { // TODO Implementér logik der validerer input (not-null, osv)
-
-                onSubmitPressed(
+        Row(modifier = Modifier
+            .padding(all = 10.dp)
+            .align(Alignment.CenterHorizontally)
+        ) {
+            Button(
+                onClick = { // TODO Implementér logik der validerer input (not-null, osv)
+                    onSubmitPressed(
                     Team(
                         1000,
                         placeholderTeamName,
-                        placeholderClubName,
+                        placeholderClubName.text,
                         placeholderCreatorName,
                         placeholderTeamUYear,
                         placeholderDivision))
-                        navController.navigate("Team")
-            }
+                        navController?.navigate("Team")
+                },
+                colors = ButtonDefaults.buttonColors(backgroundColor = PrimaryBlue, contentColor = PrimaryWhite)
 
             ) { Text("Opret Hold") }
         }
@@ -89,5 +121,5 @@ fun AddTeam(navController: NavHostController, onSubmitPressed: (Team) -> Unit ) 
 @Preview
 @Composable
 fun AddTeamPreview(){
-
+    AddTeam(navController = null, onSubmitPressed = {})
 }
